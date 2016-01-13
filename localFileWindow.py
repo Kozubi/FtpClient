6a896b51da1047245cd6b002c2ecd2a9561d12dc
Kozubi <marcink00@gmail.com>
2016-01-13 15:17:26 +0100

    - fixed image display

--- localFileWindow.py
+++ localFileWindow.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.image import Image
+#from kivy.core.image import Image
 from kivy.uix.label import Label
 from kivy.clock import Clock
 import os
@@ -43,19 +44,19 @@ kv ="""
 Builder.load_string(kv)
 
 class MyImage(Image):
-    def __init__(self, **kwargs):
-        super(MyImage, self).__init__(**kwargs)
+    def __init__(self, arg, **kwargs):
+        super(MyImage, self).__init__(arg, **kwargs)
         self.size_hint_x = 0.05
 
 class Folder(MyImage):
-    def __init__(self, **kwargs):
-        super(Folder, self).__init__(**kwargs)
-        self.source = "folder265.png"
+    def __init__(self, arg, **kwargs):
+        super(Folder, self).__init__(arg, **kwargs)
+        #self.source = "folder265.png"
 
 class File(MyImage):
-    def __init__(self, **kwargs):
-        super(File, self).__init__(**kwargs)
-        self.source = "doc.png"
+    def __init__(self, arg, **kwargs):
+        super(File, self).__init__(arg, **kwargs)
+        #self.source = "doc.png"
 
 class FileButton(BoxLayout):
     pass
@@ -67,9 +68,11 @@ class LocalFile(GridLayout):
     def __init__(self, **kwargs):
         super(LocalFile, self).__init__(**kwargs)
         #self.height = 0
+        self.POS = os.getcwd()
         Clock.schedule_once(self.sec_init, 0.1)
 
     def sec_init(self, *args):
+
         self.clear_widgets()
 
         self.files = myClass()
@@ -81,9 +84,9 @@ class LocalFile(GridLayout):
             frame = FileButton(size_hint_y=None)
             #self.image = Image(size_hint_x= .05)#, color=(0,0,0,1))
             if os.path.isdir(item):
-                img = Folder()
+                img = Image(size_hint_x=0.05, source=self.POS + "\\folder265.png")
             elif os.path.isfile(item):
-                img = File()
+                img = Image(size_hint_x=0.05,source=self.POS + "\doc.png")
 
 
             #image = Label(size_hint_x = .2, text="Image")
